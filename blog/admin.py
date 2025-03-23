668bd92f657d8396ea8fb6d02a9290934e1c77a8
Ismat <ismetsemedov@gmail.com>
2025-03-23 17:33:57 +0400

    init

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# blog/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post, Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
