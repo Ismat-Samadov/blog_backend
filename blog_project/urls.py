a4d24327b75b6e513ea5ab7ee19c6dc282c8587c
Ismat <ismetsemedov@gmail.com>
2025-03-25 11:58:35 +0400

    simple homepage

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,8 +17,27 @@ Including another URLconf
 #  blog_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.http import HttpResponse
+
+# Simple homepage view
+def home_view(request):
+    return HttpResponse("""
+    <h1>Blog Backend API</h1>
+    <p>Welcome to the Blog API. Available endpoints:</p>
+    <ul>
+        <li><a href="/api/">API Root</a></li>
+        <li><a href="/admin/">Admin Panel</a></li>
+    </ul>
+    """)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    
+    # Option 1: Redirect root to API
+    # path('', RedirectView.as_view(url='/api/')),
+    
+    # Option 2: Simple homepage (uncomment this and comment the above line to use)
+    path('', home_view, name='home'),
 ]
